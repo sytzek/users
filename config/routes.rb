238dd2d020dd3dfccc6f4d38c339090dc66cd8f6
sytze <sytzeklijnsma@yahoo.com>
2017-10-28 15:56:26 +0200

    Remove destroy action for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: %i(index new create edit update)
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
 end
